15710b2bf98b96f235c0d76c1eb173a7b5afe224
Akatsuki1910 <akatsukicirno@gmail.com>
2025-10-26 23:40:01 +0900

    Update node.ts

--- src/node.ts
+++ src/node.ts
@@ -11,7 +11,9 @@ const Main = () => {
       [...Array(55)].map((_, i) => ({
         id: i + 1,
         shape: "circularImage",
-        image: `/aspect/aspect_${i + 1}.png`,
+        image: `${import.meta.env.BUILD_BASE ?? "./"}aspect/aspect_${
+          i + 1
+        }.png`,
       }))
     );
 
