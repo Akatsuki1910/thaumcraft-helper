e1b333e22a3df54329e00fd4b556c3b0885fdc09
Akatsuki1910 <akatsukicirno@gmail.com>
2025-10-26 23:54:17 +0900

    Update node.ts

--- src/node.ts
+++ src/node.ts
@@ -1,36 +1,35 @@
 import van from "vanjs-core";
 import { DataSet } from "vis-data";
 import { Network } from "vis-network";
-import { EDGES } from "./ts/aspectUti";
+import { ASPECT_NUM, EDGES } from "./ts/aspectUti";
 
 const { main, div } = van.tags;
 
+const nodes = new DataSet(
+  ASPECT_NUM.map((v, i) => ({
+    id: v,
+    shape: "circularImage",
+    image: new URL(`/aspect/aspect_${v}.png`, import.meta.url).pathname,
+  }))
+);
+
+const edges = new DataSet(
+  EDGES.map(([from, to1, to2], i) => [
+    { id: i * 2 + 1, from, to: to1 },
+    { id: i * 2 + 2, from, to: to2 },
+  ]).flat()
+);
+
+const data = {
+  nodes: nodes,
+  edges: edges,
+};
+
 const Main = () => {
   const init = () => {
-    const nodes = new DataSet(
-      [...Array(55)].map((_, i) => ({
-        id: i + 1,
-        shape: "circularImage",
-        image: `${import.meta.env.BUILD_BASE ?? "./"}aspect/aspect_${
-          i + 1
-        }.png`,
-      }))
-    );
-
-    const edges = new DataSet(
-      EDGES.map(([from, to1, to2], i) => [
-        { id: i * 2 + 1, from, to: to1 },
-        { id: i * 2 + 2, from, to: to2 },
-      ]).flat()
-    );
-
-    // create a network
     const container = document.getElementById("node");
     if (!container) return;
-    const data = {
-      nodes: nodes,
-      edges: edges,
-    };
+
     const options = {};
     const network = new Network(container, data, options);
   };
